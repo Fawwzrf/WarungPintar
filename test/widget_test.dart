fbabc5e3cc48161ad0237efea10e5a020ee9c231
fawwzrf <fawwaz.rafi@mhs.unsoed.ac.id>
2026-04-24 16:54:11 +0700

    fix: add missing riverpod import in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:warung_pintar/main.dart';
 
 void main() {
